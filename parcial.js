95b00479c82f5498b7186c73aaa73f7dadcef402
santiagoo08 <santiago677mm@gmail.com>
2025-09-09 08:14:34 -0500

    (PAGINA ADMIN CON FUINCION JS)

---
+++ parcial.js
@@ -0,0 +1,11 @@
+document.getElementById("loginBtn").addEventListener("click", function () {
+const usuario = document.getElementById("usuario").value;
+const clave = document.getElementById("clave").value;
+
+console.log("Usuario:", usuario);
+console.log("Clave:", clave);
+alert("Datos enviados a la consola!");
+});
+
+
+
